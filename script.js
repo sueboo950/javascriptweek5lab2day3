1c3506cd33124318c928f6da848212281d3854ec
sophie950 <sueboo950950@gmail.com>
2024-06-27 02:27:12 -0400

    update

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function createMessage(){
+    let message ={}
+    message.sender = prompt("What is your name?");
+    message.request = prompt("What is your prayer request");
 
+    prayerrequestParagraph.innerHTML = `Dear ${message.sender}, Thank you for your request! Here is your prayer below..`;
+    prayerrequestParagraph.innerHTML = `Dear God, we come to you today to interseed in prayer for ${message.sender}. We ask that you grant their request ${message.request}. We thank you heavenly father for hearing us and granting their request. In Jesus name, Amen!`;
+
+}
+prayerrequestButton.onclick = createMessage
